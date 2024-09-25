f5ab04f52903cdb4aad475e457cb045cb80bdbf2
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2024-09-25 21:15:06 +0700

    Chore: Resolve issue in messageService.js

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -7,10 +7,25 @@ const IS_NEED_CS = parseInt(process.env.IS_NEED_CS) === 1
 const listContact = new Map();
 const RESET_INTERVAL_HOURS = 5 * 60 * 60 * 1000;
 
+const responseMessage = `
+🙏 Terima kasih sudah menghubungi kami! 😊
+Pesan Anda telah kami terima, dan tim kami akan segera membalasnya.
+
+Mohon bersabar ya, kami akan memberikan respon secepat mungkin. Sementara itu, jika ada hal lain yang ingin ditanyakan, jangan ragu untuk mengirimkan pesan kembali. 🙌
+
+✨ Semoga hari Anda menyenangkan! ✨`;
+
 const handleMessage = async (client, msg) => {
     const command = msg.body.trim();
     const fromNumber = msg.from.split('@')[0];
     const currentTime = Date.now();
+
+    if (msg.from.includes('@g.us')) {
+        if (DEBUG) {
+            console.log("  - [messageService] Got message from group!");
+        }
+        return;
+    }
 
     if (listContact.has(msg.from)) {
         const lastSentTime = listContact.get(msg.from);
@@ -30,14 +45,6 @@ const handleMessage = async (client, msg) => {
                     await client.sendMessage(msg.from, aiResponse);
                 } else {
                     if (!listContact.has(msg.from)) {
-                        const responseMessage = `
-🙏 Terima kasih sudah menghubungi kami! 😊
-Pesan Anda telah kami terima, dan tim kami akan segera membalasnya.
-
-Mohon bersabar ya, kami akan memberikan respon secepat mungkin. Sementara itu, jika ada hal lain yang ingin ditanyakan, jangan ragu untuk mengirimkan pesan kembali. 🙌
-
-✨ Semoga hari Anda menyenangkan! ✨`;
-
                         await client.sendMessage(msg.from, responseMessage.trim());
                         listContact.set(msg.from, currentTime);
                     }
@@ -47,6 +54,11 @@ Mohon bersabar ya, kami akan memberikan respon secepat mungkin. Sementara itu, j
                     console.error("  - [messageService] Failed to process AI response:", error);
                 }
                 await client.sendMessage(msg.from, "Maaf, terjadi kesalahan saat memproses permintaan Anda.");
+            }
+        } else {
+            if (!listContact.has(msg.from)) {
+                await client.sendMessage(msg.from, responseMessage.trim());
+                listContact.set(msg.from, currentTime);
             }
         }
         return;
@@ -60,6 +72,12 @@ Mohon bersabar ya, kami akan memberikan respon secepat mungkin. Sementara itu, j
                 console.log(`  - [messageService] Ping from ${fromNumber}`);
             }
             await msg.reply('Aku marah!! 😡');
+            break;
+        case 'ai-off':
+            if (DEBUG) {
+                console.log(`  - [messageService] AI Service OFF - Command from: ${fromNumber}`);
+            }
+            await IS_NEED_CS = 0
             break;
         default:
             console.log(`  - [messageService] Unknown command '${commandBody}' from ${fromNumber}`);
